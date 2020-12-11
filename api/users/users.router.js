2f095c360edc7b54c847bb2dd8ac9f55d538eeeb
Alejandro Sánchez <alesan.ca3@gmail.com>
2020-12-11 18:18:42 +0000

    Update users.router.js
    Get cities where the first letter matches the one givins as params

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -3,5 +3,6 @@ const controller = require("./users.controller");
 
 router.post("/register" , controller.createAcc);
 router.get("/activeUsers" , controller.activeUsers);
+router.get("/citiesList" , controller.citiesList);
 
 module.exports = router;
