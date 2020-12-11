1f7e9ca599aef76870564263a2542c724e6e0bb3
Alejandro Sánchez <alesan.ca3@gmail.com>
2020-12-11 18:04:09 +0000

    Update users.model.js
    schema name changed

--- api/users/users.model.js
+++ api/users/users.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const usersSchema = new mongoose.Schema({
     name : {
         type: String,
         required: true
