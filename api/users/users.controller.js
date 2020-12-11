e2888c725273cb61a7ad2ce7a53062e5d5acd3e0
Alejandro Sánchez <alesan.ca3@gmail.com>
2020-12-11 18:03:57 +0000

    Update users.controller.js
    Create account and get all active users added

---
+++ api/users/users.controller.js
@@ -0,0 +1,18 @@
+const usersModel = require("./users.model");
+
+module.exports.createAcc = createAcc;
+module.exports.activeUsers = activeUsers;
+
+function createAcc(req, res) {
+    return usersModel
+      .create(req.body)
+      .then(u => res.json(u))
+      .catch(e => res.status(500).json(e))
+  }
+
+  function activeUsers(req , res){
+      return usersModel.find({}).then(response => 
+        {
+            res.json(response.filter(x => x.active == true));
+        })
+  }
