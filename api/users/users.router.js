6d100fb0b90604ff27900b58209c730efdd04a84
Alejandro Sánchez <alesan.ca3@gmail.com>
2020-12-11 18:46:47 +0000

    Update users.router.js

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -4,5 +4,6 @@ const controller = require("./users.controller");
 router.post("/register" , controller.createAcc);
 router.get("/activeUsers" , controller.activeUsers);
 router.get("/citiesList" , controller.citiesList);
+router.get("/datesList" , controller.datesList);
 
 module.exports = router;
